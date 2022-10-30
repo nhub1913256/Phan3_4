1812c370f34649fa0bbdd4d89f48ab3719efeb8a
nhub1913256 <nhub1913256@student.ctu.edu.vn>
2022-10-30 21:55:12 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,29 @@
 <script >
+import AppHeader from "@/components/AppHeader.vue";
 export default{
+  components: {
+    AppHeader,
+  },
 
-}
+};
 
 
 </script>
             npm run dev
 <template>
-  <h1>Hello, Vue.js!</h1>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+  
+  
 </template>
 
-<style scoped>
+<style>
 .page{
   max-width: 400px;
   margin: auto;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
